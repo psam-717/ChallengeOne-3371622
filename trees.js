75f95acdcc39dba4d50ec86e4f78bd7978b7c4bc
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-11 01:29:39 +0000

    trees assignment

---
+++ trees.js
@@ -0,0 +1,45 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+    
+    insert(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children.has(char)) {
+                node.children.set(char, new TrieNode());
+            }
+            node = node.children.get(char);
+        }
+        node.isEnd = true;
+    }
+    
+    search(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children.has(char)) {
+                return false;
+            }
+            node = node.children.get(char);
+        }
+        return node.isEnd;
+    }
+    
+    startsWith(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+            if (!node.children.has(char)) {
+                return false;
+            }
+            node = node.children.get(char);
+        }
+        return true;
+    }
+}
