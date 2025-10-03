238c9559e20b3da22e1b6f25edd127d672e2dc58
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-03 06:12:59 +0000

    a simple user object, to work on my crud skills

---
+++ object.js
@@ -0,0 +1,15 @@
+
+// A simple user object
+const user = {
+    name: "Marvinphil Annorbah",
+    age: "10",
+    email: "mphilannorbah@gmail.com",
+    phone: ""
+}
+
+let userInputPhone = 'xxxx';
+
+user.phone = userInputPhone
+
+console.log(user)
+
